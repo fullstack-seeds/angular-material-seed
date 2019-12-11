04a9bd6a6d01747ea933ab71ad4039b0d2d9ffc1
Cheng Chang <tonychangcheng@gmail.com>
2019-12-11 17:10:26 +1100

    add scss and fix logout

--- src/app/theme/components/topnav/topnav.component.ts
+++ src/app/theme/components/topnav/topnav.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { UsersService } from '../../../core/services/users.service';
 
 @Component({
-  selector: 'app-topnav',
-  templateUrl: './topnav.component.html',
-  styleUrls: ['./topnav.component.scss']
+    selector: 'app-topnav',
+    templateUrl: './topnav.component.html',
+    styleUrls: ['./topnav.component.scss']
 })
 export class TopnavComponent implements OnInit {
+    public pushRightClass: string;
 
-  constructor() { }
+    constructor(public router: Router, private usersService: UsersService) {
+        this.router.events.subscribe(val => {
+            if (val instanceof NavigationEnd && window.innerWidth <= 992 && this.isToggled()) {
+                this.toggleSidebar();
+            }
+        });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.pushRightClass = 'push-right';
+    }
 
+    isToggled(): boolean {
+        const dom: Element = document.querySelector('body');
+        return dom.classList.contains(this.pushRightClass);
+    }
+
+    toggleSidebar() {
+        const dom: any = document.querySelector('body');
+        dom.classList.toggle(this.pushRightClass);
+    }
+
+    onLoggedout() {
+        this.usersService.logout()
+        this.router.navigate(['/login']);
+    }
 }
